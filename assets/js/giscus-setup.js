9577b71fe1fe9fe47e48c843aaa117fb173854d5
satyam-aw <satyamawasthi007@gmail.com>
2026-05-22 14:44:20 +0000

    Deploying to gh-pages from @ satyam-aw/satyam-aw.github.io@37a574c758a9cf352b90a3f63a16f816e14634a6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "satyam-aw/personal-website",
+    "data-repo": "satyam-aw/satyam-aw.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
