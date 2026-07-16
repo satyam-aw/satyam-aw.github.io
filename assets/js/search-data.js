2d6c852277928692ddb083668b579491e7910882
satyam-aw <satyamawasthi007@gmail.com>
2026-07-16 13:49:34 +0000

    Deploying to gh-pages from @ satyam-aw/satyam-aw.github.io@f1168166f8de4aa1876b7f179c12a7e570f07c00 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-zen-garden",
           title: "zen garden",
-          description: "A quiet place for my growing plant collection gathered from hiking trips across California. The photographs from 2021–2023 showcase species from Santa Barbara County, documented while I was pursuing my Master&#39;s degree at UC Santa Barbara. After graduating, I relocated north. The plants listed under 2023–2026 are native species discovered along the hiking trails around the Bay Area.",
+          description: "A quiet place for my growing plant collection gathered from hiking trips across California. The photographs from 2021–2023 showcase species from Santa Barbara County, documented while I was pursuing my Master&#39;s degree at UC Santa Barbara. After graduating, I relocated north. The plants listed under 2023–2026 are native species discovered along the hiking trails around the Bay Area. The garden is also home to two interactive snails- Garry, moves away when the cursor gets too close, while Stinky curiously moves toward it.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/zen-garden/";
